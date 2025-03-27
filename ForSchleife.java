d85f962c3cfe1e6a153d0215c00572f835effdac
ASchultz <131669459+Adprivat@users.noreply.github.com>
2025-03-27 13:16:41 +0100

    Schleifen

---
+++ ForSchleife.java
@@ -0,0 +1,9 @@
+public class ForSchleife {
+    public static void main(String[] args) {
+        System.out.println("Ausgabe mit for-Schleife:");
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+} 
---
+++ WhileSchleife.java
@@ -0,0 +1,11 @@
+public class WhileSchleife {
+    public static void main(String[] args) {
+        System.out.println("Ausgabe mit while-Schleife:");
+        
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+} 
